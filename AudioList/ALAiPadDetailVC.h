a445844e1162302697ce8687c96963f171a623c1
Derek Weber <derek.weber03@gmail.com>
2014-05-09 09:54:00 -0400

    ALA - added DetailVC & ListVC

---
+++ AudioList/ALAiPadDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  ALAiPadDetailVC.h
+//  AudioList
+//
+//  Created by Derek Weber on 5/9/14.
+//  Copyright (c) 2014 Derek Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAiPadDetailVC : UIViewController
+
+@end
---
+++ AudioList/ALAiPadListTableVC.h
@@ -0,0 +1,13 @@
+//
+//  ALAiPadListTableVC.h
+//  AudioList
+//
+//  Created by Derek Weber on 5/9/14.
+//  Copyright (c) 2014 Derek Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAiPadListTableVC : UITableViewController
+
+@end
